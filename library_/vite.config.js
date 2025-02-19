8556115d2e275b89a9418fe2c517a0c4cb686f91
mahmoud elbalhi <mahmoud01234elbalhi@gmail.com>
2025-02-19 12:42:04 +0200

    up

--- library_/vite.config.js
+++ library_/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/Library_website/', // Set this to the name of your GitHub repository
+  plugins: [react()],
+  server:{
+    host:true,
+  }
+
 })
